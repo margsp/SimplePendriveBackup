47b337b29abffa87f3a752afd43105a6da8c49ac
Mariusz Gasperaniec <135326711+margsp@users.noreply.github.com>
2025-04-18 14:12:15 +0200

    POC

--- Program.cs
+++ Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string file1 = @"C:\plik1.txt";
+            string file2 = @"C:\plik2.txt";
+            string targetLocation1 = @"D:\plik1.txt";
+            string targetLocation2 = @"D:\plik2.txt";
+
+            File.Copy(file1, targetLocation1, true);
+            File.Copy(file2, targetLocation2, true);
+
+            Console.WriteLine("Done. Press any key...");
+            Console.ReadKey();
         }
     }
 }
